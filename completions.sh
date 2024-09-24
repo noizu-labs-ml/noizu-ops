833fbffbde49a7b9e86f3b066e20cd405013a5eb
Keith Brings <keith.brings@noizu.com>
2024-09-24 16:02:44 +0700

    newbase

--- completions.sh
+++
@@ -1,8 +0,0 @@
-# Completions for noizu-ops
-_noizu_ops_completions() {
-    local cur="${COMP_WORDS[COMP_CWORD]}"
-    local commands=("query" "--session" "help")
-    COMPREPLY=( $(compgen -W "${commands[*]}" -- "${cur}") )
-}
-
-complete -F _noizu_help_completions noizu-ops
